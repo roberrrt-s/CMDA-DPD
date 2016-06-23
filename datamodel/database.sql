529a6cff170575e88513e43e9104ca869863e781
Robert Spier <robert.spier@gmail.com>
2016-06-23 13:36:47 +0200

    updated datamodel

--- datamodel/database.sql
+++ datamodel/database.sql
@@ -43,6 +43,7 @@ ENGINE = InnoDB;
 -- Table `dpd`.`slideshow`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `dpd`.`slideshow` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NULL,
   PRIMARY KEY (`id`))
