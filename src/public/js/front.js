b11f62a2411414f40b12346a72d167dbb2145ea6
Robert Spier <robert.spier@gmail.com>
2016-07-05 16:59:35 +0200

    commented code for #7

--- src/public/js/front.js
+++ src/public/js/front.js
@@ -1,5 +1,6 @@
 var video = (function(){
 
+	// Insert the YouTube API.
 	var init = function() {
 		var tag = document.createElement('script');
 		tag.src = "https://www.youtube.com/player_api";
@@ -7,15 +8,19 @@ var video = (function(){
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);			
 	}
 
+	// When it's ready, it fires the onYouTubePlayerAPIReady() function
 	var ready = function(videoId, element) {
 
+		// Declare the max/min height/width
 		var body = document.body,
 			html = document.documentElement,
 			totalHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight),
 			totalWidth = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
 
+		// Select all video containers
 		var containers = document.querySelectorAll('.video > div')
 
+		// Insert the YouTube iframe for every container
 		for(var i = 0; i < containers.length; i++) {
 			var videoId = containers[i].getAttribute('data-video')
 			var element = containers[i].id
@@ -23,6 +28,7 @@ var video = (function(){
 		}
 	}
 
+	// Create a new YouTube iframe and insert it
 	var insert = function(videoId, element, tHeight, tWidth) {
 		var player = new YT.Player(element, {
 			height: tHeight,
@@ -31,6 +37,7 @@ var video = (function(){
 		});				
 	}
 
+	// Return the public functions
 	return {
 		init: init,
 		ready: ready,
